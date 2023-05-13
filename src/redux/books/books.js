ed27652e182306aed7aa804abd1352d88a3ee2a0
Fickry Iman <fickry.iman@gmail.com>
2023-05-13 11:12:12 +0700

    put addcase to the next line

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -25,39 +25,43 @@ const removeBook = createAsyncThunk(REMOVE_BOOK, async (id) => {
 });
 
 const initialState = {
-  status: null,
   bookList: [],
+  status: null,
 };
 
 const handleBookSlice = createSlice({
   name: 'bookSlice',
   initialState,
+  reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(addBook.fulfilled, (state, action) => {
-      state.bookList.push({
-        id: action.payload.item_id,
-        title: action.payload.title,
-        author: action.payload.author,
-        category: action.payload.category,
-      });
-    });
-    builder.addCase(getBooks.fulfilled, (state, action) => {
-      const newBookList = [];
-      Object.entries(action.payload).forEach((item) => {
-        newBookList.push({
-          id: item[0],
-          title: item[1][0].title,
-          author: item[1][0].author,
-          category: item[1][0].category,
+    builder
+      .addCase(addBook.fulfilled, (state, action) => {
+        state.bookList.push({
+          id: action.payload.item_id,
+          title: action.payload.title,
+          author: action.payload.author,
+          category: action.payload.category,
         });
       });
-      // eslint-disable-next-line no-param-reassign
-      state.bookList = newBookList;
-    });
-    builder.addCase(removeBook.fulfilled, (state, action) => {
-      // eslint-disable-next-line no-param-reassign
-      state.bookList = state.bookList.filter((book) => book.id !== action.payload);
-    });
+    builder
+      .addCase(getBooks.fulfilled, (state, action) => {
+        const newBookList = [];
+        Object.entries(action.payload).forEach((item) => {
+          newBookList.push({
+            id: item[0],
+            title: item[1][0].title,
+            author: item[1][0].author,
+            category: item[1][0].category,
+          });
+        });
+        // eslint-disable-next-line no-param-reassign
+        state.bookList = newBookList;
+      });
+    builder
+      .addCase(removeBook.fulfilled, (state, action) => {
+        // eslint-disable-next-line no-param-reassign
+        state.bookList = state.bookList.filter((book) => book.id !== action.payload);
+      });
   },
 });
 
